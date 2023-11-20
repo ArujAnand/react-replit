ccda217589ac4b9d3db90f6ab8d5734ae4f21aa0
Aruj <arujanand@protonmail.com>
2023-11-20 13:41:24 +0530

    Made navbar and homepage component separate

--- src/App.js
+++ src/App.js
@@ -2,21 +2,21 @@ import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import {HashRouter, Routes, Route} from "react-router-dom";
 
-import Nav from "./components/Home"
-import { About, Contact, Doctor, Services,} from './components/Main';
+import Nav from "./components/Nav"
+import {Contact, Doctor, Services, About} from './components/Main';
 
-// import Content from './components/Content'; used for displaying navbar separate and homepage separate
+import Content from './components/Content'; 
 import {Login} from "./components/Login.jsx";
 import {Signup} from "./components/Signup.jsx";
-// import Login from './components/Login.js';
 
 function App() {
   return (
     <div>
        <HashRouter>
       {/* <Header /> */}
+      <Nav />
         <Routes>
-          <Route path="/" element={<Nav />}/>
+          <Route path="/" element={<Content />}/>
           <Route path="/Services" element={<Services />}/>
           <Route path="/About" element={<About />}/>
           <Route path="/Doctor" element={<Doctor />}/>
